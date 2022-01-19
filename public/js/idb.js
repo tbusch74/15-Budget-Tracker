6370146932521f45cd01dcaf3d115f85139a982b
Tony <abusch@css.edu>
2022-01-18 23:08:47 -0600

    indexedDB

---
+++ public/js/idb.js
@@ -0,0 +1,59 @@
+let db;
+const request = indexedDB.open('budget-tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('transaction', { autoIncrement: true });
+  };
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+      uploadTransaction();
+    }
+  };
+  
+  request.onerror = function(event) {
+    console.log(event.target.errorCode);
+  };
+
+function saveRecord(record) {
+    const transaction = db.transaction(['transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('transaction');
+    transactionObjectStore.add(record);
+  }
+
+function uploadTransaction() {
+    const transaction = db.transaction(['transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('transaction');
+    const getAll = transactionObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(['transaction'], 'readwrite');
+          const transactionObjectStore = transaction.objectStore('transaction');
+          transactionObjectStore.clear();
+
+          alert('All saved transactions have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };  
+}
+
+window.addEventListener('online', uploadTransaction);
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
 const app = express();
 
 app.use(logger("dev"));
@@ -16,9 +14,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
